86a0910e42a85fc4ac8e80cec5349635f2257241
Jenkibear <jenkinwork123@gmail.com>
2024-04-20 09:59:49 +1000

    navbar adjustments

--- git-explorer/src/components/navbar.js
+++ git-explorer/src/components/navbar.js
@@ -3,7 +3,7 @@ import "./styles.css";
 import { Outlet, Link } from "react-router-dom";
 
 
-const Navbar = () => {
+const Navbar = ({isLogged}) => {
   return (
     <>
       <nav>
@@ -13,6 +13,9 @@ const Navbar = () => {
         <div className="link-cont">
           <Link to="/">Repos</Link>
           <Link to="/users">Users</Link>
+          <Link to={'/search'}>Search</Link>
+          <Link to={'/authProfile'}>Profile</Link>
+          {!isLogged&&<Link to={'/login'}>Login</Link>}
         </div>
       </nav>
       <Outlet />
