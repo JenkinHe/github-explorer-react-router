c0bee485ce18476a8f48bec90fb7ac629740ad22
Jenkibear <jenkinwork123@gmail.com>
2024-04-20 10:41:14 +1000

    lazy loading

--- git-explorer/src/App.js
+++ git-explorer/src/App.js
@@ -2,16 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes, Navigate,useLocation } from "react-router-dom";
 import Home from "./components/home";
-import AboutUs from "./components/about";
-import Users from "./components/users";
-import Navbar from "./components/navbar";
-import NotFound from "./components/notfound";
-import UserProfile from "./components/userProfile"
-import SearchUser from "./components/searchUser"
-import Login from "./components/login";
-import AuthProfile from './components/authProfile';
-import { useState } from "react";
+import { useState,lazy ,Suspense} from "react";
 import { CSSTransition, SwitchTransition } from "react-transition-group"
+
+const Users=lazy(()=>import("./components/users"));
+const UserProfile=lazy(()=>import("./components/userProfile"));
+const SearchUser=lazy(()=>import("./components/searchUser"));
+const Login=lazy(()=>import("./components/login"));
+const AuthProfile=lazy(()=>import('./components/authProfile'));
+const AboutUs=lazy(()=>import("./components/about"));
+const NotFound=lazy(()=>import("./components/notfound"));
 
 function App() {
   const [username, setUsername] = useState('');
@@ -20,6 +20,7 @@ function App() {
   return (
     <SwitchTransition component={null}>
       <CSSTransition key={location.pathname} classNames="fade" timeout={300} unmountOnExit>
+        <Suspense fallback={()=><h1>Loading...</h1>}>
         <div className="App">
           <Routes location={location}>
             
@@ -37,6 +38,7 @@ function App() {
 
           </Routes>
         </div>
+        </Suspense>
 
       </CSSTransition>
     </SwitchTransition>
