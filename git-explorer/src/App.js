fa41780ca3ff7cd96428f70b2d65bc838a5eacfa
Jenkibear <jenkinwork123@gmail.com>
2024-04-20 10:28:37 +1000

    navigation animation transition

--- git-explorer/src/App.js
+++ git-explorer/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Route, Routes,Navigate } from "react-router-dom";
+import { Route, Routes, Navigate,useLocation } from "react-router-dom";
 import Home from "./components/home";
 import AboutUs from "./components/about";
 import Users from "./components/users";
@@ -11,28 +11,36 @@ import SearchUser from "./components/searchUser"
 import Login from "./components/login";
 import AuthProfile from './components/authProfile';
 import { useState } from "react";
+import { CSSTransition, SwitchTransition } from "react-transition-group"
 
 function App() {
-  const [username,setUsername] =useState('');
-  const [isLogged,setIsLogged] =useState(false);
+  const [username, setUsername] = useState('');
+  const [isLogged, setIsLogged] = useState(false);
+  const location = useLocation();
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Navbar isLogged={isLogged} />}>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/users/user/:username" element={<UserProfile/>}/>
-          <Route path="/search" element={<SearchUser/>}/>
-          <Route element={<Login setIsLogged={setIsLogged} setUsername={setUsername}/>} path="/login"/>
-          <Route element={isLogged? <AuthProfile username={username}/>:<Navigate replace to={"/login"}/>} path="/authProfile"/>
+    <SwitchTransition component={null}>
+      <CSSTransition key={location.pathname} classNames="fade" timeout={300} unmountOnExit>
+        <div className="App">
+          <Routes location={location}>
+            
+              <Route exact path="/" element={<Home />} />
+              <Route path="/about" element={<AboutUs />} />
+              <Route path="/users" element={<Users />} />
+              <Route path="/users/user/:username" element={<UserProfile />} />
+              <Route path="/search" element={<SearchUser />} />
+              <Route element={<Login setIsLogged={setIsLogged} setUsername={setUsername} />} path="/login" />
+              <Route element={isLogged ? <AuthProfile username={username} /> : <Navigate replace to={"/login"} />} path="/authProfile" />
 
-        </Route>
-        <Route path="*" element={<NotFound/>}/>
+            
+            <Route path="*" element={<NotFound />} />
 
 
-      </Routes>
-    </div>
+          </Routes>
+        </div>
+
+      </CSSTransition>
+    </SwitchTransition>
+
   );
 }
 
